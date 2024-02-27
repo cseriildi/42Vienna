172bfbafcc877931d00eb50dd6a28ad8a3f3c2a9
Ildiko Cseri <icseri@c2r3p5.42vienna.com>
2024-02-27 16:34:48 +0100

    setting up the files

---
+++ piscine/c08/ex00/ft.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: icseri <icseri@student.your42network>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 16:31:33 by icseri            #+#    #+#             */
+/*   Updated: 2024/02/27 16:31:51 by icseri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_putchar(char c);
+void ft_swap(int *a, int *b);
+void ft_putstr(char *str);
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2);
