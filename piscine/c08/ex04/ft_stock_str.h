a4d3fdefe75117cb8980578d62d9f20586a2d257
Ildiko Cseri <icseri@c2r3p4.42vienna.com>
2024-02-28 16:20:09 +0100

    update solutions

--- piscine/c08/ex04/ft_stock_str.h
+++ piscine/c08/ex04/ft_stock_str.h
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: icseri <icseri@student.your42network>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/27 16:32:13 by icseri            #+#    #+#             */
-/*   Updated: 2024/02/27 16:32:16 by icseri           ###   ########.fr       */
+/*   Created: 2024/02/28 13:05:12 by icseri            #+#    #+#             */
+/*   Updated: 2024/02/28 13:05:52 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_BOOLEAN_H
-# define FT_BOOLEAN_H
-
-# include <unistd.h>
-
-typedef int	t_bool;
-# define TRUE 1
-# define FALSE 0
-# define SUCCESS 0
-# define EVEN(nbr) (nbr % 2 == 0)
-# define EVEN_MSG "I have an even number of arguments.\n"
-# define ODD_MSG "I have an odd number of arguments.\n"
-
-#endif
+typedef struct	s_stock_str
+{
+	int	size;
+	char	*str;
+	char	*copy;
+}	t_stock_str;
