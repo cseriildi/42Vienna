d0b6697d9fd96c231cfef7cc8b8edb353f716296
cseriildi <cseriildiko@icloud.com>
2024-02-28 16:20:09 +0100

    update solutions

--- piscine/c08/ex00/ft.h
+++ piscine/c08/ex00/ft.h
@@ -9,14 +9,13 @@
 /*   Updated: 2024/02/27 16:31:51 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #ifndef FT_H
-#define FT_H
+# define FT_H
 
-void ft_putchar(char c);
-void ft_swap(int *a, int *b);
-void ft_putstr(char *str);
-int ft_strlen(char *str);
-int ft_strcmp(char *s1, char *s2);
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
 
 #endif
--- piscine/c08/ex04/ft_strs_to_tab.c
+++ piscine/c08/ex04/ft_strs_to_tab.c
@@ -10,4 +10,54 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_stock_str.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	dest = malloc(sizeof(src));
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int	i;
+	struct s_stock_str	arr;
+	
+	i = 0;
+	while (i < ac)
+	{
+		arr.size[i] = ft_strlen(av[i]);
+		arr.str[i] = ft_strcpy(arr.str[i], av[i]);
+		arr.copy[i] = ft_strcpy(arr.copy[i], arr.str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	ft_strs_to_tab(argc, argv);
+	
+}
