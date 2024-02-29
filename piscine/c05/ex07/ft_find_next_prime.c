50686fb748f705e77730d61a64e3139e9b6b935c
Ildiko Cseri <icseri@c2r3p5.42vienna.com>
2024-02-29 17:28:19 +0100

    adding solution for c08 and updating c05 c06 c07

--- piscine/c05/ex07/ft_find_next_prime.c
+++ piscine/c05/ex07/ft_find_next_prime.c
@@ -6,31 +6,41 @@
 /*   By: icseri <icseri@student.your42network>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 11:22:21 by icseri            #+#    #+#             */
-/*   Updated: 2024/02/23 12:20:47 by icseri           ###   ########.fr       */
+/*   Updated: 2024/02/29 11:01:05 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_find_next_prime(int nb)
+int	ft_is_prime(int nb)
 {
 	int	i;
 
 	i = 2;
 	if (nb > 1)
 	{
-		while (i * i <= nb)
+		while (i * i <= nb && i <= 46340)
 		{
 			if (nb % i == 0)
-				return (ft_find_next_prime(nb + 1));
+				return (0);
 			i++;
 		}
-		return (nb);
+		return (1);
 	}
-	return (2);
+	return (0);
 }
 
+int	ft_find_next_prime(int nb)
+{
+	if (nb <= 2)
+		return (2);
+	else if (ft_is_prime(nb))
+		return (nb);
+	else
+		return (ft_find_next_prime(nb + 1));
+}
+/*
 int	main(int argc, char **argv)
 {
 	int	i;
@@ -38,8 +48,8 @@ int	main(int argc, char **argv)
 	i = 1;
 	while (i < argc)
 	{
-		printf("\nThe next prime greater or equeal than (%s", argv[i]);
-		printf(" is  %d", ft_find_next_prime(atoi(argv[i])));
+		printf("\nThe next prime greater than or equal to %s", argv[i]);
+		printf(" is %d", ft_find_next_prime(atoi(argv[i])));
 		i++;
 	}
-}
+}*/
