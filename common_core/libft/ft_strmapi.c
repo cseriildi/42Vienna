52f3cfd926a26ded9f6b262a20117db09419554d
Ildiko Cseri <icseri@c2r4p3.42vienna.com>
2024-04-05 10:34:12 +0200

    update ft_strmapi and ft_striteri

--- common_core/libft/ft_strmapi.c
+++ common_core/libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 16:34:51 by icseri            #+#    #+#             */
-/*   Updated: 2024/04/04 16:47:24 by icseri           ###   ########.fr       */
+/*   Updated: 2024/04/05 10:23:53 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,32 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	char	*new_s;
+	int		i;
+
+	new_s = malloc(ft_strlen(s) + 1);
+	if (new_s == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new_s[i] = f(i, s[i]);
+		i++;
+	}
+	new_s[i] = '\0';
+	return (new_s);
 }
+/* 
+char	test_function(unsigned int x, char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s \n", ft_strmapi(argv[1], test_function));
+}
+ */
