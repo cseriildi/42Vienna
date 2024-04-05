fdc9031424a265c421810189142c383c3f44f2b6
Ildiko Cseri <icseri@c2r4p3.42vienna.com>
2024-04-05 09:53:55 +0200

    add all additional functions

--- common_core/libft/ft_strmapi.c
+++ common_core/libft/ft_strmapi.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/03 13:55:15 by icseri            #+#    #+#             */
-/*   Updated: 2024/04/04 16:29:47 by icseri           ###   ########.fr       */
+/*   Created: 2024/04/04 16:34:51 by icseri            #+#    #+#             */
+/*   Updated: 2024/04/04 16:47:24 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrchr(const char *s, int c)
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	size_t	len;
-
-	len = ft_strlen(s) - 1;
-	while (len >= 0)
-	{
-		if (s[len] == c)
-			return ((char *) s + len);
-		len--;
-	}
-	return (0);
+	
 }
-/*
-int	main(int argc, char **argv)
-{
-	(void)argc;
-	printf("Original function: %s \n", strrchr(argv[1], argv[2][0]));
-	printf("My function: %s \n", ft_strrchr(argv[1], argv[2][0]));
-}*/
