ba6829002a69163b6a2ae54115020a4fab836bcd
Ildiko Cseri <icseri@c2r3p5.42vienna.com>
2024-04-09 12:07:35 +0200

    final updates

--- common_core/libft/ft_substr.c
+++ common_core/libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 10:14:16 by icseri            #+#    #+#             */
-/*   Updated: 2024/04/04 16:14:36 by icseri           ###   ########.fr       */
+/*   Updated: 2024/04/09 10:54:02 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*substr;
 	size_t			i;
-	unsigned int	slen;
+	unsigned int	str_len;
 
-	slen = ft_strlen(s);
 	i = 0;
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+		len = 0;
+	else if (str_len - start < len)
+		len = str_len;
 	substr = malloc(len + 1);
 	if (substr == NULL)
 		return (NULL);
-	while (len-- > 0 && start < slen)
+	while (len-- > 0)
 		substr[i++] = s[start++];
 	substr[i] = '\0';
 	return (substr);
@@ -31,6 +35,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 /* 
 int	main(int argc, char **argv)
 {
-	printf("%s\n", ft_substr(argv[1], atoi(argv[2]), atoi(argv[3])));
+	char	*str;
+
+	str = ft_substr(argv[1], atoi(argv[2]), atoi(argv[3]));
+	printf("%s\n", str);
+	free(str);
 }
  */
