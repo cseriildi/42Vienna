6d54b8277a50f42f07380b846ffb4f1ae59fd8df
Ildiko Cseri <icseri@c2r3p5.42vienna.com>
2024-04-09 14:05:07 +0200

    fixing small malloc issues

--- common_core/libft/ft_split.c
+++ common_core/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 08:55:08 by icseri            #+#    #+#             */
-/*   Updated: 2024/04/09 11:54:21 by icseri           ###   ########.fr       */
+/*   Updated: 2024/04/09 14:02:53 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,20 +49,25 @@ static char	**create_empty_array(char const *str, char c)
 	char	**words;
 	int		len;
 
-	len = ft_strlen(str);
 	count = occ(str, c);
 	words = malloc(sizeof(char *) * (count + 1));
 	if (words == NULL)
-		return (ft_free(words), NULL);
-	i = 0;
-	while (i < count)
+		return (NULL);
+	i = -1;
+	while (++i < count)
 	{
+		len = 0;
+		while (*str && *str == c)
+			str++;
+		while (*str && *str != c)
+		{
+			str++;
+			len++;
+		}
 		words[i] = malloc(len + 1);
 		if (words[i] == NULL)
 			return (ft_free(words), NULL);
-		i++;
 	}
-	words[i] = NULL;
 	return (words);
 }
 
@@ -74,7 +79,7 @@ char	**ft_split(char const *str, char c)
 
 	words = create_empty_array(str, c);
 	if (words == NULL)
-		return (ft_free(words), NULL);
+		return (NULL);
 	i = 0;
 	j = 0;
 	while (*str)
@@ -94,8 +99,8 @@ char	**ft_split(char const *str, char c)
 	words[i] = NULL;
 	return (words);
 }
-/* 
-int	main(int argc, char **argv)
+
+/* int	main(int argc, char **argv)
 {
 	char	**words;
 	int		i;
@@ -107,5 +112,4 @@ int	main(int argc, char **argv)
 		printf("[%s]\n", words[i++]);
 	printf("[%s]\n", words[i]);
 	ft_free(words);
-}
-*/
+} */
