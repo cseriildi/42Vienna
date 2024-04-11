1a47dbbfd84769b66778203c72f8f3b7bab5bde1
Ildiko Cseri <icseri@c2r3p3.42vienna.com>
2024-04-11 16:17:34 +0200

    rewriteing logic from scratch

--- common_core/libft/ft_itoa.c
+++ common_core/libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 12:52:33 by icseri            #+#    #+#             */
-/*   Updated: 2024/04/11 10:42:48 by icseri           ###   ########.fr       */
+/*   Updated: 2024/04/11 16:14:30 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,61 @@
 
 static int	ft_nbrlen(int n)
 {
-	int	i;
+	int	digit;
 
-	i = 0;
-	if (n <= 0)
-		i++;
-	while (n != 0)
+	digit = 1;
+	if (n < 0)
+	{
+		if (n / -10 == 0)
+			return (digit);
+		else
+		{
+			n /= -10;
+			digit++;
+		}
+	}
+	while (n / 10 != 0)
 	{
 		n /= 10;
-		i++;
+		digit++;
 	}
-	return (i);
+	return (digit);
 }
 
-static int	is_neg(int n)
+static int	ft_power(int nb, int power)
 {
-	if (n <= 0)
-		return (-1);
-	return (1);
+	if (power < 0)
+		return (0);
+	else if (power == 0)
+		return (1);
+	else
+		return (nb * ft_power(nb, power - 1));
 }
 
 char	*ft_itoa(int nb)
 {
 	char	*nbr_as_str;
+	int		dig;
+	int		curr;
+	int		sign;
 	int		i;
 
-	i = ft_nbrlen(nb);
-	nbr_as_str = ft_calloc(i + 1, 1);
+	sign = ft_power(-1, (nb < 0));
+	dig = ft_nbrlen(nb);
+	nbr_as_str = ft_calloc(dig + 1 + (sign == -1), 1);
 	if (nbr_as_str == NULL)
 		return (NULL);
-	if (nb == 0)
-		nbr_as_str[0] = '0';
-	if (nb < 0)
-		nbr_as_str[0] = '-';
-	while (nb != 0)
+	i = (sign == -1);
+	while (dig-- > 0)
 	{
-		nbr_as_str[--i] = ((nb % (10 * is_neg(nb))) * is_neg(nb)) + '0';
-		nb /= (10 * is_neg(nb));
+		if (sign == -1)
+			nbr_as_str[0] = '-';
+		curr = nb / (ft_power(10, dig) * sign);
+		nbr_as_str[i++] = curr + '0';
+		if (curr == 0)
+			curr = 1;
+		nb = (nb % (curr * ft_power(10, dig) * sign)) * sign;
+		sign *= sign;
 	}
 	return (nbr_as_str);
 }
