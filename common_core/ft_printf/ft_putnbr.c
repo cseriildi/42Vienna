d4db64e2fcf7bce7f25481ffe4589a355eb0d98f
Ildiko Cseri <icseri@c2r3p3.42vienna.com>
2024-04-15 12:44:37 +0200

    almost finished state

--- common_core/ft_printf/ft_putnbr.c
+++ common_core/ft_printf/ft_putnbr.c
@@ -1,41 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 14:51:53 by icseri            #+#    #+#             */
-/*   Updated: 2024/04/12 22:21:14 by icseri           ###   ########.fr       */
+/*   Updated: 2024/04/15 11:54:17 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_nbrlen(long n, int base)
+static int	ft_nbrlen(int n)
 {
 	int	digit;
 
 	digit = 1;
 	if (n < 0)
 	{
-		if (n / (base * -1) == 0)
+		if (n / -10 == 0)
 			return (digit);
 		else
 		{
-			n /= (base * -1);
+			n /= -10;
 			digit++;
 		}
 	}
-	while (n / base != 0)
+	while (n / 10 != 0)
 	{
-		n /= base;
+		n /= 10;
 		digit++;
 	}
 	return (digit);
 }
 
-static long	ft_power(long nb, long power)
+static int	ft_power(int nb, int power)
 {
 	if (power < 0)
 		return (0);
@@ -45,44 +45,36 @@ static long	ft_power(long nb, long power)
 		return (nb * ft_power(nb, power - 1));
 }
 
-int	ft_putnbr_base(long nb, char *baseformat)
+int	ft_putnbr(int nb)
 {
 	int	dig;
 	int	curr;
+	int	count;
 	int	sign;
-	int	base;
-	int	counter;
 
-	counter = 0;
-	base = ft_strlen(baseformat);
 	sign = 1;
 	if (nb < 0)
 		sign = -1;
-	dig = ft_nbrlen(nb, base);
+	count = 0;
+	dig = ft_nbrlen(nb);
 	while (dig > 0)
 	{
 		if (sign == -1)
-			counter += ft_putchar('-');
-		curr = nb / (ft_power(base, dig - 1) * sign);
-		counter += ft_putchar(baseformat[curr]);
+			count += ft_putchar('-');
+		curr = nb / (ft_power(10, dig - 1) * sign);
+		count += ft_putchar(curr + '0');
 		if (curr == 0)
 			curr = 1;
-		nb = (nb % (curr * ft_power(base, dig - 1) * sign)) * sign;
+		nb = (nb % (curr * ft_power(10, dig - 1) * sign)) * sign;
 		dig--;
 		sign *= sign;
 	}
-	return (counter);
+	return (count);
 }
 /* 
 int	main(int argc, char **argv)
 {
-	char *p;
-	
-	p = "0";
 	(void)argc;
-	ft_putnbr_base(atoi(argv[1]), argv[2]));
-	ft_putstr_fd("\n", 1);
-	ft_putnbr_base((uintptr_t)&p, argv[2]));
-
+	ft_putnbr_fd(atoi(argv[1]), atoi(argv[2]));
 }
  */
