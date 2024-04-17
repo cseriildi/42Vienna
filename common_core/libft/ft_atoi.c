24fb0096437a327e6bd4d04234adb5cc5fcd8f80
cseriildi <cseriildiko@icloud.com>
2024-04-17 15:25:31 +0200

    fix: if nbr overflows the long size return 0 or -1 based on sign

--- common_core/libft/ft_atoi.c
+++ common_core/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 17:17:48 by icseri            #+#    #+#             */
-/*   Updated: 2024/04/04 16:21:47 by icseri           ###   ########.fr       */
+/*   Updated: 2024/04/17 15:21:48 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	ft_atoi(const char *nptr)
 {
-	int	sign;
-	int	nbr;
+	int		sign;
+	int		nbr;
+	long	l;
 
 	nbr = 0;
+	l = 0;
 	while (*nptr == ' ' || (*nptr >= 9 && *nptr <= 13))
 		nptr++;
 	sign = 1;
@@ -29,17 +31,18 @@ int	ft_atoi(const char *nptr)
 	}
 	while (*nptr >= '0' && *nptr <= '9')
 	{
-		nbr *= 10;
-		nbr += *nptr - 48;
-		nptr++;
+		if (l != (l * 10 + *nptr - 48) / 10)
+			return ((sign >= 0) * -1);
+		l = l * 10 + *nptr - '0';
+		nbr = nbr * 10 + *nptr++ - '0';
 	}
 	return (sign * nbr);
 }
-/*
-int	main(void)
+/* 
+int	main(int argc, char **argv)
 {
-	const char *nbr = "  -123B32";
-
-	printf("Original function: %d \n", atoi(nbr));
-	printf("My function: %d \n", ft_atoi(nbr));
-}*/
+	(void)argc;
+	printf("Original function: %d \n", atoi(argv[1]));
+	printf("My function: %d \n", ft_atoi(argv[1]));
+}
+ */
