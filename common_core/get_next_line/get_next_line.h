43fcf0ed6a4823188effd6fa0159f4c4c5deeee4
icseri <icseri@student.42vienna.com>
2024-04-22 16:50:27 +0200

    remove unnecessary includes

--- common_core/get_next_line/get_next_line.h
+++ common_core/get_next_line/get_next_line.h
@@ -6,19 +6,20 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 15:27:17 by icseri            #+#    #+#             */
-/*   Updated: 2024/04/22 16:33:48 by icseri           ###   ########.fr       */
+/*   Updated: 2024/04/22 16:49:36 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <sys/stat.h>
+/* 
 # include <fcntl.h>
-# include <stdint.h>
+# include <stdio.h>
+*/
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
