3521ea69e8977857dc8fd202b846c6029cf22740
icseri <cseriildiko@icloud.com>
2024-04-22 16:42:53 +0200

    final version

--- common_core/get_next_line/get_next_line.c
+++ common_core/get_next_line/get_next_line.c
@@ -6,42 +6,59 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 15:27:07 by icseri            #+#    #+#             */
-/*   Updated: 2024/04/19 16:47:30 by icseri           ###   ########.fr       */
+/*   Updated: 2024/04/22 16:35:55 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char	*read_fd(int fd, char *read_chars)
+{
+	char		*tmp;
+	char		*buffer;
+	long		read_size;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (buffer == NULL)
+		return (ft_free(&read_chars), NULL);
+	read_size = 1;
+	while (read_size > 0 && is_nl(read_chars) == 0)
+	{
+		read_size = read(fd, buffer, BUFFER_SIZE);
+		if (read_size == -1)
+			return (ft_free(&buffer), ft_free(&read_chars), NULL);
+		buffer[read_size] = '\0';
+		tmp = ft_strjoin(read_chars, buffer);
+		ft_free(&read_chars);
+		read_chars = tmp;
+		if (!read_chars)
+			return (ft_free(&buffer), ft_free(&read_chars), NULL);
+	}
+	ft_free(&buffer);
+	if (!read_chars || !read_chars[0])
+		return (ft_free(&read_chars), NULL);
+	return (read_chars);
+}
 
 char	*get_next_line(int fd)
 {
 	static char	*read_chars;
 	char		*line;
 	char		*tmp;
-	char		*buffer;
-	int			read_size;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = malloc(BUFFER_SIZE + 1);
-	if (buffer == NULL)
-		return (NULL);
-	while (is_nl(read_chars) == 0)
-	{
-		read_size = read(fd, buffer, BUFFER_SIZE);
-		if (read_size == -1)
-			return (free(buffer),free(read_chars), NULL);
-		if (read_size == 0)
-			break ;
-		buffer[read_size] = '\0';
-		tmp = ft_strjoin(read_chars, buffer);
-		free(read_chars);
-		read_chars = tmp;
-	}
-	free(buffer);
-	if (!read_chars || !read_chars[0])
-		return (free(read_chars), NULL);
+	read_chars = read_fd(fd, read_chars);
+	if (!read_chars)
+		return (ft_free(&read_chars), NULL);
 	line = line_search(read_chars);
- 	read_chars = remove_line(read_chars);
+	if (!line || !*line)
+		return (ft_free(&read_chars), ft_free(&line), NULL);
+	tmp = ft_strjoin(NULL, read_chars + ft_strlen(line));
+	ft_free(&read_chars);
+	read_chars = tmp;
+	if (!read_chars)
+		return (ft_free(&line), ft_free(&read_chars), NULL);
 	return (line);
 }
 /* 
@@ -54,16 +71,7 @@ int	main(void)
 	if (fd == -1)
 		return (-1);
 	line = get_next_line(fd);
-	printf("[%s]\n", line);
-	free(line);
-	line = get_next_line(fd);
-	printf("[%s]\n", line);
-	free(line);
-	close(fd);
-	fd = open("test.txt", O_RDWR);
-	if (fd == -1)
-		return (-1);
-	line = get_next_line(fd);
+
 	while (line)
 	{
 		printf("[%s]\n", line);
--- common_core/get_next_line/test.txt
+++
@@ -1,4 +0,0 @@
-aaaaaaaaaa
-bbbbbbbbbb
-cccccccccc
-dddddddddd
