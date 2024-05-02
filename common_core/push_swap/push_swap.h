f4247dd05e33c0c2cc8069d8c9138aecec05cc20
icseri <cseriildiko@icloud.com>
2024-05-02 18:57:39 +0200

    implementing circular linked list, a little malloc issue is present

--- common_core/push_swap/push_swap.h
+++ common_core/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 17:06:31 by icseri            #+#    #+#             */
-/*   Updated: 2024/04/30 17:07:19 by icseri           ###   ########.fr       */
+/*   Updated: 2024/05/02 15:18:55 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,17 @@
 
 # include <unistd.h>
 # include "libft/libft.h"
+
+typedef struct s_clist
+{
+	void			*content;
+	struct s_clist	*next;
+	struct s_clist	*previous;
+}	t_clist;
+
+t_clist	*ft_circ_lstnew(void *content);
+void	ft_circ_lstadd_front(t_clist **lst, t_clist *new);
+void	ft_circ_lstdelone(t_clist **lst);
+void	ft_circ_lstclear(t_clist **lst);
 
 #endif
