bf5cddc532c1c55073a6787d1b6febb5a00e9e8d
cseriildi <cseriildiko@icloud.com>
2024-05-03 17:00:27 +0200

    started working on bonus

---
+++ common_core/push_swap/checker_bonus.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker_bonus.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 15:57:56 by icseri            #+#    #+#             */
+/*   Updated: 2024/05/03 16:57:22 by icseri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHECKER_BONUS_H
+# define CHECKER_BONUS_H
+
+# include "push_swap.h"
+
+int	checker(int count, int *numbers, char **instructions);
+
+#endif
