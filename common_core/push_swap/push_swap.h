44434295f51d925e296e418126e78ed822ac0792
cseriildi <cseriildiko@icloud.com>
2024-05-03 17:02:18 +0200

    defining sorting rules and check if sorted

--- common_core/push_swap/push_swap.h
+++ common_core/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 17:06:31 by icseri            #+#    #+#             */
-/*   Updated: 2024/05/03 13:14:47 by icseri           ###   ########.fr       */
+/*   Updated: 2024/05/03 16:59:25 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,26 @@ typedef struct s_clist
 	struct s_clist	*previous;
 }	t_clist;
 
+//list
 t_clist	*ft_circ_lstnew(void *content);
 void	ft_circ_lstadd_front(t_clist **lst, t_clist *new);
 void	ft_circ_lstdelone(t_clist **lst);
 void	ft_circ_lstclear(t_clist **lst);
 
+//push_swap
 void	ft_free(char **p);
 int		check_input(int count, char **input);
 t_clist	**create_stack(int count, int *numbers);
 int		push_swap(int count, int *numbers);
 
+//operations
 void	swap(t_clist **stack);
 void	push(t_clist **stack_src, t_clist **stack_dest);
 void	rotate(t_clist **head);
 void	rev_rotate(t_clist **head);
+
+//sorting
+int		check_if_sorted(t_clist **stack_a, t_clist **stack_b);
+void	sorting(t_clist **stack_a, t_clist **stack_b, char *rule);
 
 #endif
