746391f354c13767f1712765830eb949eab0f559
icseri <icseri@student.42vienna.com>
2024-05-06 12:23:49 +0200

    fixing errors, finishing bonus

--- common_core/push_swap/ft_printf/ft_putstr.c
+++ common_core/push_swap/ft_printf/ft_putstr.c
@@ -1,21 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker_bonus.h                                    :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/03 15:57:56 by icseri            #+#    #+#             */
-/*   Updated: 2024/05/06 10:55:35 by icseri           ###   ########.fr       */
+/*   Created: 2024/04/04 13:44:55 by icseri            #+#    #+#             */
+/*   Updated: 2024/04/15 13:21:37 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CHECKER_BONUS_H
-# define CHECKER_BONUS_H
+#include "ft_printf.h"
 
-# include "push_swap.h"
-# include "gnl/get_next_line.h"
+static size_t	ft_strlen(const char *s)
+{
+	size_t	len;
 
-int	checker(t_clist	**stack_a);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
 
-#endif
+int	ft_putstr(char *s)
+{
+	if (!s)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	else
+	{
+		write(1, s, ft_strlen(s));
+		return (ft_strlen(s));
+	}
+}
+/* 
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	ft_putstr(argv[1]);
+} */
