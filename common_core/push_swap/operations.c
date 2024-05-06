7b35c1d65fc6ec403b4cdea8a93eb924ab09d230
icseri <icseri@student.42vienna.com>
2024-05-06 20:28:18 +0200

    mostly working code, but there are still things to fix

--- common_core/push_swap/operations.c
+++ common_core/push_swap/operations.c
@@ -6,7 +6,7 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 11:06:38 by icseri            #+#    #+#             */
-/*   Updated: 2024/05/06 10:17:04 by icseri           ###   ########.fr       */
+/*   Updated: 2024/05/06 19:42:36 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	swap(t_clist **stack)
 	int		tmp_content;
 	int		tmp_rank;
 
+	if (!stack && !*stack)
+		return ;
 	head = *stack;
 	next = head->next;
 	tmp_content = head->content;
@@ -33,8 +35,14 @@ void	push(t_clist **stack_src, t_clist **stack_dest)
 {
 	t_clist	*head;
 	t_clist	*head_copy;
+	t_clist *next;
 
+	if (!*stack_src)
+		return ;
 	head = *stack_src;
+	next = head->next;
+	if (head == next)
+		next = NULL;
 	head_copy = ft_circ_lstnew(head->content, head->rank);
 	if (!head_copy)
 	{
@@ -43,16 +51,17 @@ void	push(t_clist **stack_src, t_clist **stack_dest)
 	}
 	ft_circ_lstadd_front(stack_dest, head_copy);
 	ft_circ_lstdelone(&head);
+	*stack_src = next;
 }
 
-void	rotate(t_clist **head)
+void	rotate(t_clist **stack)
 {
-	if (*head != NULL)
-		*head = (*head)->next;
+	if (stack && *stack)
+		*stack = (*stack)->next;
 }
 
-void	rev_rotate(t_clist **head)
+void	rev_rotate(t_clist **stack)
 {
-	if (*head != NULL)
-		*head = (*head)->previous;
+	if (stack && *stack)
+		*stack = (*stack)->previous;
 }
