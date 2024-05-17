02d29447f534701e03e8a95786c735aae4f0682d
icseri <icseri@student.42vienna.com>
2024-05-17 14:12:05 +0200

    restart

--- common_core/pipex/main.c
+++ common_core/pipex/main.c
@@ -3,25 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cseriildii <cseriildii@student.42.fr>      +#+  +:+       +#+        */
+/*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 13:53:45 by cseriildii        #+#    #+#             */
-/*   Updated: 2024/05/12 20:22:48 by cseriildii       ###   ########.fr       */
+/*   Updated: 2024/05/17 14:11:38 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+/* int	main(int argc, char **argv)
 {
-	int		fd;
+	int		fd[2];
+	int		pid;
 	char	*infile;
 	char	*outfile;
 	char	*command;
 	int		i;
 
 	if (argc  < 5)
-		return (perror("Not enough parameters\n"), 1);
+		return (write(2, "Not enough parameters\n", 23), 1);
+	if (pipe(fd) == -1)
+		return (perror("Error occured while opening pipe\n"), 2);
+	pid = fork();
+	if (pid == -1) 
+		return (perror("Error occured while forking\n"), 3);
 	infile = argv[1];
 	outfile = argv[argc - 1];
 	i = 2;
@@ -30,7 +36,7 @@ int	main(int argc, char **argv)
 		command = argv[i++];
 	}
 	if (access(infile, F_OK) == -1)
-		return (perror("Infile does not exist\n"), 1);
+		return (perror("Infile does not exist\n"), 2);
 	fd = open(infile, O_RDONLY);
 	if (fd == -1)
 		return (perror("Infile exists but not readable\n"), 1);
@@ -39,4 +45,4 @@ int	main(int argc, char **argv)
 	if (fd == -1)
 		return (1);
 	close(fd);
-	}
+	} */
