ec305688f7d6d02d253960c825225ce29e6b583b
cseriildi <cseriildiko@icloud.com>
2024-05-22 23:30:41 +0200

    still leaking

--- common_core/pipex/pipex.c
+++ common_core/pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: cseriildii <cseriildii@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:57:19 by icseri            #+#    #+#             */
-/*   Updated: 2024/05/22 21:34:31 by cseriildii       ###   ########.fr       */
+/*   Updated: 2024/05/22 23:24:02 by cseriildii       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,44 +21,47 @@ void	exec_command(char *cmd, char **env)
 	int		i;
 
 	i = 0;
-	args = ft_split(cmd, ' ');
-	if (!args)
-		exit(2);
 	while (env[i] && ft_strncmp(env[i], "PATH=", 5) != 0)
 		i++;
+	if (!env[i])
+	{
+		perror("PATH not found");
+		exit(EXIT_FAILURE);
+	}
 	path = ft_split(env[i] + 5, ':');
 	if (!path)
+		exit(EXIT_FAILURE);
+	args = ft_split(cmd, ' ');
+	if (!args)
 	{
-		array_free(&args);
-		exit(3);
+		array_free(&path);
+		exit(EXIT_FAILURE);
 	}
-	i = 0;
-	while (path[i] && *args)
+	i = -1;
+	while (path[++i] && *args)
 	{
 		tmp = ft_strjoin(path[i], "/");
 		if (!tmp)
 		{
 			array_free(&path);
 			array_free(&args);
-			exit(4);
+			exit(EXIT_FAILURE);
 		}
 		full_path = ft_strjoin(tmp, args[0]);
+		free(tmp);
 		if (!full_path)
 		{
 			array_free(&path);
 			array_free(&args);
-			free(tmp);
-			exit(4);
+			exit(EXIT_FAILURE);
 		}
-		free(tmp);
 		execve(full_path, args, env);
 		free(full_path);
-		i++;
 	}
 	array_free(&args);
 	array_free(&path);
-	perror("Command does not exist\n");
-	exit(1);
+	perror("Command does not exist");
+	exit(EXIT_FAILURE);
 }
 
 void	first_command(int *pipe_fd, char *infile, char *cmd, char **env)
@@ -68,15 +71,21 @@ void	first_command(int *pipe_fd, char *infile, char *cmd, char **env)
 	fd = open(infile, O_RDONLY, 0777);
 	if (fd == -1)
 	{
-		perror("Could not open file\n");
-		exit(1);
+		perror("Could not open file");
+		exit(EXIT_FAILURE);
 	}
 	close(pipe_fd[0]);
 	if (dup2(fd, STDIN_FILENO) == -1)
-		exit(1);
+	{
+		perror("Error while dup2");
+		exit(EXIT_FAILURE);
+	}
 	close(fd);
 	if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
-		exit(1);
+	{
+		perror("Error while dup2");
+		exit(EXIT_FAILURE);
+	}
 	close(pipe_fd[1]);
 	exec_command(cmd, env);
 }
@@ -89,14 +98,20 @@ void	last_command(int *pipe_fd, char *outfile, char *cmd, char **env)
 	if (fd == -1)
 	{
 		perror("Could not open/create outfile");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	close(pipe_fd[1]);
 	if (dup2(pipe_fd[0], STDIN_FILENO) == -1)
-		exit(1);
+	{
+		perror("Error while dup2");
+		exit(EXIT_FAILURE);
+	}
 	close(pipe_fd[0]);
 	if (dup2(fd, STDOUT_FILENO) == -1)
-		exit(1);
+	{
+		perror("Error while dup2");
+		exit(EXIT_FAILURE);
+	}
 	close(fd);
 	exec_command(cmd, env);
 }
@@ -112,11 +127,11 @@ int	main(int argc, char **argv, char **env)
 		return (1);
 	}
 	if (pipe(pipe_fd) == -1)
-		return (perror("Could not open pipe\n"), 1);
+		return (perror("Could not open pipe"), 1);
 	pid = fork();
 	if (pid == -1)
 	{
-		perror("Error occured while forking\n");
+		perror("Error occured while forking");
 		return (close(pipe_fd[0]), close(pipe_fd[1]), 1);
 	}
 	else if (pid == 0)
