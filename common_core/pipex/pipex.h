8a527f13e83f6bda4c8a2546599738436bea6066
cseriildi <cseriildiko@icloud.com>
2024-05-22 17:19:42 +0200

    working as expected, edge case handeling needed and malloc check

--- common_core/pipex/pipex.h
+++ common_core/pipex/pipex.h
@@ -6,10 +6,9 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 14:01:26 by cseriildii        #+#    #+#             */
-/*   Updated: 2024/05/15 14:22:56 by icseri           ###   ########.fr       */
+/*   Updated: 2024/05/22 12:52:21 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef PIPEX_H
 # define PIPEX_H
@@ -19,10 +18,11 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 # include <errno.h>
+# include <stdio.h>
+# include <sys/wait.h>
 
 # include "libft/libft.h"
-# include "ft_printf/ft_printf.h"
-# include "gnl/get_next_line.h"
 
+void	exec_command(char *cmd, char **env);
 
 #endif
