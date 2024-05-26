0e6a86b1554d774e5e06263912057fd00412b880
cseriildi <cseriildiko@icloud.com>
2024-05-26 15:42:46 +0200

    final touches on the mandatory part, starting on the bonus

--- common_core/pipex/parsing.c
+++ common_core/pipex/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cseriildii <cseriildii@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 18:15:55 by icseri            #+#    #+#             */
-/*   Updated: 2024/05/24 18:25:34 by icseri           ###   ########.fr       */
+/*   Updated: 2024/05/26 15:22:31 by cseriildii       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	parse_input(t_var *data, int count, char **params, char **env)
 	data->cmd_count = count - 3;
 	data->commands = params + 2;
 	data->commands[data->cmd_count] = NULL;
+	data->absolut_cmd = NULL;
 	data->pipe_fd[0] = -1;
 	data->pipe_fd[1] = -1;
 	data->env = env;
@@ -28,13 +29,13 @@ void	parse_input(t_var *data, int count, char **params, char **env)
 	while (data->env[i] && ft_strncmp(data->env[i], "PATH=", 5) != 0)
 		i++;
 	if (!data->env[i])
-		elegant_exit("PATH not found", data);
+		elegant_exit(data, MISSING_PATH);
 	data->path = ft_split(data->env[i] + 5, ':');
 	if (!data->path)
-		elegant_exit("Memory allocation failed", data);
+		elegant_exit(data, MALLOC_FAIL);
 }
 
-void	find_path(t_var *data, int cmd_index)
+char	*find_path(t_var *data)
 {
 	int		i;
 	char	*abs_cmd;
@@ -42,27 +43,12 @@ void	find_path(t_var *data, int cmd_index)
 	i = -1;
 	while (data->path[++i])
 	{
-		abs_cmd = ft_strjoin2(data->path[i], data->commands[cmd_index], "/");
+		abs_cmd = ft_strjoin2(data->path[i], data->cmd, "/");
 		if (!abs_cmd)
-			elegant_exit("Memory allocation failed", data);
-		if (access(abs_cmd, F_OK | X_OK) == 0)
-		{
-			data->absolut_cmd = abs_cmd;
-			return ;
-		}
+			elegant_exit(data, MALLOC_FAIL);
+		if (access(abs_cmd, F_OK) == 0)
+			return (abs_cmd);
 		free(abs_cmd);
 	}
-	elegant_exit("Command does not exist", data);
-}
-
-void	set_command(t_var *data, int cmd_index)
-{
-	data->args = ft_split(data->commands[cmd_index], ' ');
-	if (!data->args)
-		elegant_exit("Memory allocation failed", data);
-	data->cmd = data->args[0];
-	if (access(data->cmd, F_OK | X_OK) == 0)
-		data->absolut_cmd = data->cmd;
-	else
-		find_path(data, cmd_index);
+	return (NULL);
 }
--- common_core/pipex/utils.c
+++ common_core/pipex/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cseriildii <cseriildii@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 21:14:03 by cseriildii        #+#    #+#             */
-/*   Updated: 2024/05/24 18:12:22 by icseri           ###   ########.fr       */
+/*   Updated: 2024/05/25 20:08:48 by cseriildii       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,6 @@ void	array_free(char ***arr)
 	}
 }
 
-void	elegant_exit(char *error_msg, t_var *data)
-{
-	if (data && data->pipe_fd[0] != -1)
-		close(data->pipe_fd[0]);
-	if (data && data->pipe_fd[1] != -1)
-		close(data->pipe_fd[1]);
-	perror(error_msg);
-	exit(EXIT_FAILURE);
-}
-
 char	*ft_strjoin2(char *str1, char *str2, char *delimiter)
 {
 	char	*tmp;
@@ -48,4 +38,48 @@ char	*ft_strjoin2(char *str1, char *str2, char *delimiter)
 	if (!new_str)
 		return (NULL);
 	return (new_str);
+}
+
+char	*error_message(int code)
+{
+	if (code == ERROR_MISUSE)
+		return ("The number of arguments are incorrect");
+	else if (code == CANNOT_OPEN_FILE)
+		return ("Unable to open the file");
+	else if (code == MALLOC_FAIL)
+		return ("Memory allocation failed");
+	else if (code == MISSING_PATH)
+		return ("PATH not found");
+	else if (code == PIPE_FAIL)
+		return ("Pipe failed");
+	else if (code == DUP2_FAIL)
+		return ("Duplication failed");
+	else if (code == FORK_FAIL)
+		return ("Fork failed");
+	else if (code == ERROR_CANNOT_EXECUTE)
+		return ("Command found but not executable");
+	else if (code == COMMAND_NOT_FOUND)
+		return ("Command not found");
+	else
+		return ("Unknown error");
+}
+
+void	elegant_exit(t_var *data, int error_code)
+{
+	perror(error_message(error_code));
+	if (data->path)
+		array_free(&data->path);
+	if (data->args)
+		array_free(&data->args);
+	if (data->absolut_cmd)
+		ft_free(&data->absolut_cmd);
+	if (data->pipe_fd[0] != -1)
+		close(data->pipe_fd[0]);
+	if (data->pipe_fd[1] != -1)
+		close(data->pipe_fd[1]);
+	close(data->infile_fd);
+	close(data->outfile_fd);
+	if (data)
+		free(data);
+	exit(error_code);
 }
