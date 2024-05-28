2c6bf229d45009d66a80da44b0019bb883882b0b
icseri <cseriildiko@icloud.com>
2024-05-28 19:07:06 +0200

    not working at the moment

--- common_core/pipex/pipex.c
+++ common_core/pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:57:19 by icseri            #+#    #+#             */
-/*   Updated: 2024/05/27 18:48:46 by icseri           ###   ########.fr       */
+/*   Updated: 2024/05/28 15:40:40 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ void	first_command(t_var *data)
 	data->i_fd = open(data->infile, O_RDONLY, 0777);
 	if (data->i_fd == -1)
 		elegant_exit(data, CANNOT_OPEN_FILE);
-	close(data->pipe_fd[0]);
+	close(data->pipe[0]);
 	if (dup2(data->i_fd, STDIN_FILENO) == -1)
 		elegant_exit(data, DUP2_FAIL);
 	close(data->i_fd);
-	if (dup2(data->pipe_fd[1], STDOUT_FILENO) == -1)
+	if (dup2(data->pipe[1], STDOUT_FILENO) == -1)
 		elegant_exit(data, DUP2_FAIL);
-	close(data->pipe_fd[1]);
+	close(data->pipe[1]);
 	exec_command(data, 0);
 }
 
@@ -32,10 +32,10 @@ void	last_command(t_var *data)
 	data->o_fd = open(data->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (data->o_fd == -1)
 		elegant_exit(data, CANNOT_OPEN_FILE);
-	close(data->pipe_fd[1]);
-	if (dup2(data->pipe_fd[0], STDIN_FILENO) == -1)
+	close(data->pipe[1]);
+	if (dup2(data->pipe[0], STDIN_FILENO) == -1)
 		elegant_exit(data, DUP2_FAIL);
-	close(data->pipe_fd[0]);
+	close(data->pipe[0]);
 	if (dup2(data->o_fd, STDOUT_FILENO) == -1)
 		elegant_exit(data, DUP2_FAIL);
 	close(data->o_fd);
@@ -52,7 +52,7 @@ int	main(int argc, char **argv, char **env)
 	if (!data)
 		elegant_exit(NULL, MALLOC_FAIL);
 	parse_input(data, argc, argv, env);
-	if (pipe(data->pipe_fd) == -1)
+	if (pipe(data->pipe) == -1)
 		elegant_exit(data, PIPE_FAIL);
 	data->pid = fork();
 	if (data->pid == -1)
@@ -61,5 +61,4 @@ int	main(int argc, char **argv, char **env)
 		first_command(data);
 	else
 		last_command(data);
-	return (0);
 }
