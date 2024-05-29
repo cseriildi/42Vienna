87880e773484c847eda6711c0fdca6452f6a8cad
icseri <cseriildiko@icloud.com>
2024-05-29 18:51:42 +0200

    final version

--- common_core/pipex/utils.c
+++ common_core/pipex/utils.c
@@ -6,30 +6,11 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 21:14:03 by cseriildii        #+#    #+#             */
-/*   Updated: 2024/05/29 11:28:15 by icseri           ###   ########.fr       */
+/*   Updated: 2024/05/29 18:45:25 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-void	exec_command(t_var *data, int cmd_index)
-{
-	data->args = ft_split(data->commands[cmd_index], ' ');
-	if (!data->args)
-		elegant_exit(data, MALLOC_FAIL);
-	data->cmd = data->args[0];
-	if (access(data->cmd, X_OK) == 0)
-		data->absolut_cmd = data->cmd;
-	else
-		data->absolut_cmd = find_path(data);
-	if (execve(data->absolut_cmd, data->args, data->env) == -1)
-	{
-		if (data->absolut_cmd == NULL)
-			elegant_exit(data, COMMAND_NOT_FOUND);
-		else
-			elegant_exit(data, ERROR_CANNOT_EXECUTE);
-	}
-}
 
 void	array_free(char **arr)
 {
@@ -59,59 +40,38 @@ char	*ft_strjoin2(char *str1, char *str2, char *delimiter)
 	return (new_str);
 }
 
-char	*error_message(int code)
-{
-	if (code == ERROR_MISUSE)
-		return ("The number of arguments are incorrect");
-	else if (code == CANNOT_OPEN_FILE)
-		return ("Unable to open the file");
-	else if (code == MALLOC_FAIL)
-		return ("Memory allocation failed");
-	else if (code == MISSING_PATH)
-		return ("PATH not found");
-	else if (code == PIPE_FAIL)
-		return ("Pipe failed");
-	else if (code == DUP2_FAIL)
-		return ("Duplication failed");
-	else if (code == FORK_FAIL)
-		return ("Fork failed");
-	else if (code == ERROR_CANNOT_EXECUTE)
-		return ("Command found but not executable");
-	else if (code == COMMAND_NOT_FOUND)
-		return ("Command not found");
-	else if (code == UNLINK_FAIL)
-		return ("Can not delete file");
-	else
-		return ("Unknown error");
-}
-
-void	close_pipe(int fd)
+void	safe_close(int fd)
 {
 	if (fd != -1)
 		close(fd);
 	fd = -1;
 }
 
-void	elegant_exit(t_var *data, int error_code)
+int	safe_open(char *filename, char mode, t_var *data)
 {
-	perror(error_message(error_code));
-	if (data)
-	{
-		if (data->path)
-			array_free(data->path);
-		if (data->args)
-			array_free(data->args);
-		if (data->absolut_cmd)
-			ft_free(&data->absolut_cmd);
-		if (data->limiter)
-			ft_free(&data->limiter);
-		close_pipe(data->pipe[0]);
-		close_pipe(data->pipe[1]);
-		close_pipe(data->i_fd);
-		close_pipe(data->o_fd);
-		close_pipe(data->pipe2[0]);
-		close_pipe(data->pipe2[1]);
-		free(data);
-	}
-	exit(error_code);
+	int	fd;
+
+	fd = -1;
+	if (mode == 'R')
+		fd = open(filename, O_RDONLY, 0777);
+	else if (mode == 'W')
+		fd = open(filename, O_WRONLY, 0777);
+	else if (mode == 'C')
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	else if (mode == 'A')
+		fd = open(filename, O_WRONLY | O_APPEND, 0777);
+	else
+		elegant_exit(data, ERROR_MISUSE);
+	if (fd == -1)
+		elegant_exit(data, CANNOT_OPEN_FILE);
+	return (fd);
+}
+
+void	create_process(t_var *data, int fd[2])
+{
+	if (pipe(fd) == -1)
+		elegant_exit(data, PIPE_FAIL);
+	data->pid = fork();
+	if (data->pid == -1)
+		elegant_exit(data, FORK_FAIL);
 }
