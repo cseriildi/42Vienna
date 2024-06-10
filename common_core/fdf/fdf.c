2e81037d3ec3a95bedd870666ac55f7562110aad
cseriildi <cseriildiko@icloud.com>
2024-06-10 07:44:08 +0200

    parsing is done

--- common_core/fdf/fdf.c
+++ common_core/fdf/fdf.c
@@ -6,8 +6,37 @@
 /*   By: cseriildii <cseriildii@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 17:47:06 by cseriildii        #+#    #+#             */
-/*   Updated: 2024/06/06 17:48:05 by cseriildii       ###   ########.fr       */
+/*   Updated: 2024/06/09 10:54:38 by cseriildii       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+int	main(int argc, char **argv)
+{
+	t_var *data;
+
+	if (argc != 2)
+		return (ft_putendl_fd("Missing argument", 2), 1);
+	data = malloc(sizeof(t_var));
+	if (!data)
+		return (ft_putendl_fd("Malloc failed", 2), 2);
+	data->filename = argv[1];
+	parsing(data);
+
+
+	int i = 0;
+	int j = 0;
+
+	while (i < data->height)
+	{
+		j = 0;
+		while ( j < data->width)
+		{
+			ft_printf("%d ", data->map[i][j]);
+			j++;
+		}
+		ft_printf("\n");
+		i++;
+	}
+}
--- common_core/fdf/fdf.h
+++ common_core/fdf/fdf.h
@@ -6,11 +6,39 @@
 /*   By: cseriildii <cseriildii@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 17:47:15 by cseriildii        #+#    #+#             */
-/*   Updated: 2024/06/06 17:47:43 by cseriildii       ###   ########.fr       */
+/*   Updated: 2024/06/09 10:50:26 by cseriildii       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <stdlib.h> 
+
+/* typedef enum {
+    EXIT_SUCCESS,
+    EXIT_FAILURE,
+    ERROR_MISUSE,
+    CANNOT_OPEN_FILE,
+    MALLOC_FAIL
+} ErrorCodes; */
+
+
+typedef struct s_var
+{
+	char	*filename;
+	char	*content;
+	char	**str_map;
+	int 	**map;
+	int		width;
+	int		height;
+}	t_var;
+
+
+void	parsing(t_var	*data);
+
+
 
 #endif
