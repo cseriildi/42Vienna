899ce38072cffe9b109196a32b7c9e17c3e1d3a9
icseri <icseri@student.42vienna.com>
2024-06-14 17:13:29 +0200

    started implementing linked list for the bonus

---
+++ common_core/get_next_line/get_next_line_bonus.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 15:27:17 by icseri            #+#    #+#             */
+/*   Updated: 2024/06/14 14:39:02 by icseri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+
+typedef struct s_list
+{
+	int				fd;
+	char			*content;
+	struct s_list	*next;
+}	t_list;
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *s);
+char	*line_search(char *s);
+int		is_nl(char *read_chars);
+void	ft_free(char **p);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+
+# endif
+
+#endif
