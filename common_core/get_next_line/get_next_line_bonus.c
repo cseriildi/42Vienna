bcd4e6728d8212b62b7b733393d947692b0aa78b
icseri <cseriildiko@icloud.com>
2024-06-14 17:13:29 +0200

    started implementing linked list for the bonus

--- common_core/get_next_line/get_next_line_bonus.c
+++ common_core/get_next_line/get_next_line_bonus.c
@@ -6,11 +6,34 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 15:27:07 by icseri            #+#    #+#             */
-/*   Updated: 2024/06/14 13:47:40 by icseri           ###   ########.fr       */
+/*   Updated: 2024/06/14 17:10:14 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
+
+t_list	*ft_lstnew(int	fd)
+{
+	t_list	*new_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->fd = fd;
+	new_node->content = NULL;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst && new)
+	{
+		new->next = *lst;
+		*lst = new;
+	}
+}
+
 
 static char	*read_fd(int fd, char *read_chars)
 {
@@ -42,27 +65,41 @@ static char	*read_fd(int fd, char *read_chars)
 
 char	*get_next_line(int fd)
 {
-	static char	*read_chars[1024];
-	char		*line;
-	char		*tmp;
+	static t_list	**list;
+	t_list			*read_chars;
+	char			*line;
+	char			*tmp;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 1024)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	read_chars[fd] = read_fd(fd, read_chars[fd]);
-	if (!read_chars[fd])
-		return (ft_free(&read_chars[fd]), NULL);
-	line = line_search(read_chars[fd]);
+	read_chars = NULL;
+	if (list)
+		read_chars = *list;
+	while (read_chars && read_chars->fd != fd)
+		read_chars = read_chars->next;
+	if (read_chars == NULL)
+	{
+		list = malloc(sizeof(t_list));
+		ft_lstadd_front(list, ft_lstnew(fd));
+	}
+	read_chars->content = read_fd(fd, read_chars->content);
+	if (!read_chars->content)
+		return (ft_free(&read_chars->content), NULL);
+	line = line_search(read_chars->content);
 	if (!line || !*line)
-		return (ft_free(&read_chars[fd]), ft_free(&line), NULL);
-	tmp = ft_strjoin(NULL, read_chars[fd] + ft_strlen(line));
-	ft_free(&read_chars[fd]);
-	read_chars[fd] = tmp;
-	if (!read_chars[fd])
-		return (ft_free(&line), ft_free(&read_chars[fd]), NULL);
+		return (ft_free(&read_chars->content), ft_free(&line), NULL);
+	tmp = ft_strjoin(NULL, read_chars->content + ft_strlen(line));
+	ft_free(&read_chars->content);
+	read_chars->content = tmp;
+	if (!read_chars->content)
+		return (ft_free(&line), ft_free(&read_chars->content), NULL);
 	return (line);
 }
 
-/* int	main(void)
+# include <fcntl.h>
+# include <stdio.h>
+
+int	main(void)
 {
 	int		fd;
 	int		fd2;
@@ -94,4 +131,3 @@ char	*get_next_line(int fd)
 	}
 	close(fd);
 }
- */
