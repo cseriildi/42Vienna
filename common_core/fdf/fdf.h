90459fca51f8b0466b7743c95382740cfe7172dd
cseriildi <cseriildiko@icloud.com>
2024-06-18 11:58:25 +0200

    still not working

--- common_core/fdf/fdf.h
+++ common_core/fdf/fdf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cseriildii <cseriildii@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 17:47:15 by cseriildii        #+#    #+#             */
-/*   Updated: 2024/06/17 19:28:14 by icseri           ###   ########.fr       */
+/*   Updated: 2024/06/18 11:34:08 by cseriildii       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 # include "libft/libft.h"
 # include <fcntl.h>
 # include <stdlib.h> 
-# include <mlx.h>
+# include "mlx/mlx.h"
+//# include <mlx.h>
 # include <math.h>
 
 # define ESC 65307
@@ -25,6 +26,7 @@
 # define WIDTH 1090
 # define HEIGHT 960
 # define ANGLE 30
+# define BASE_COLOR -1
 
 # define PI 3.14159265358979323846
 
@@ -62,7 +64,7 @@ typedef struct s_var
 	t_point	**map_2d;
 	int		width;
 	int		height;
-	int	angle;
+	int		angle;
 	float	scale;
 	void	*display;
 	void	*window;
@@ -82,8 +84,13 @@ void	array_free(t_point **arr);
 void	char_array_free(char **arr);
 
 void	create_map(t_var *data);
-//iso
 
+//iso
 void	create_2d(t_var *data);
+
+//atoi base
+int	ft_atoi_base(const char *str, char *base);
+
+void	draw(t_var *data);
 
 #endif
--- common_core/fdf/iso_proj.c
+++ common_core/fdf/iso_proj.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iso_proj.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cseriildii <cseriildii@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 11:47:42 by icseri            #+#    #+#             */
-/*   Updated: 2024/06/14 11:48:03 by icseri           ###   ########.fr       */
+/*   Updated: 2024/06/18 11:44:50 by cseriildii       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,23 @@ double rad(int angle)
 	return (angle * PI / 180);
 }
 
-void	convert_to_2d(t_point p3d, t_2d_point p2d)
+void convert_to_2d(t_point *p3d, t_point *p2d)
 {
-	p2d.x = (p3d.x - p3d.y) * cos(rad(ANGLE));
-    p2d.y = (p3d.x + p3d.y) * sin(rad(ANGLE)) - p3d.z;
+    p2d->x = (p3d->x - p3d->y) * cos(rad(ANGLE));
+    p2d->y = (p3d->x + p3d->y) * sin(rad(ANGLE)) - p3d->z;
+    p2d->color = p3d->color;
 }
 
-void	create_2d(t_var *data)
+void create_2d(t_var *data)
 {
-	int column;
-	int row;
+    int column;
+    int row;
 
-	row = -1;
-	while (++row < data->height)
-	{
-		column = -1;
-		while (++column < data->width)
-			convert_to_2d(data->map[row][column], data->map_2d[row][column]);
-	}
+    row = -1;
+    while (++row < data->height)
+    {
+        column = -1;
+        while (++column < data->width)
+            convert_to_2d(&data->map[row][column], &data->map_2d[row][column]);
+    }
 }
