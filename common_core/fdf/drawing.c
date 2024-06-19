83b489b2cb8098693779cffb90aa43aacb7917cc
icseri <icseri@student.42vienna.com>
2024-06-19 16:15:02 +0200

    issue at the iso_proj.c

--- common_core/fdf/drawing.c
+++ common_core/fdf/drawing.c
@@ -6,54 +6,52 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 08:13:22 by cseriildii        #+#    #+#             */
-/*   Updated: 2024/06/18 12:04:36 by icseri           ###   ########.fr       */
+/*   Updated: 2024/06/19 15:58:45 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	draw_line(t_point p1, t_point p2, t_var *data)
+void draw_line(t_point p1, t_point p2, t_var *data)
 {
-	int dx;
-	int dy;
-	int error;
-	int error2;
-	int sign_x;
-	int sign_y;
-	int x;
-	int y;
+    double dx = abs(p2.x - p1.x);
+    double dy = abs(p2.y - p1.y);
+    int sign_x = p1.x < p2.x ? 1 : -1;
+    int sign_y = p1.y < p2.y ? 1 : -1;
+    double error = dx - dy;
+    double error2;
 
-	dx = abs(p2.x - p1.x);
-	dy = abs(p2.y - p1.y);
-	sign_x = p1.x < p2.x ? 1 : -1;
-	sign_y = p1.y < p2.y ? 1 : -1;
-	error = dx - dy;
-	x = p1.x;
-	y = p1.y;
-	while (x != p2.x || y != p2.y)
-	{
-		if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
-			*(unsigned int *)(data->image.addr + (y * data->image.line_len + x * (data->image.bpp / 8))) = p1.color;
-		error2 = error * 2;
-		if (error2 > -dy)
-		{
-			error -= dy;
-			x += sign_x;
-		}
-		if (error2 < dx)
-		{
-			error += dx;
-			y += sign_y;
-		}
-	}
+    while (p1.x != p2.x || p1.y != p2.y)
+    {
+        if (p1.x >= 0 && p1.x <= WIDTH && p1.y >= 0 && p1.y <= HEIGHT)
+            *(unsigned int *)(data->image.addr + (p1.y * data->image.line_len + p1.x * (data->image.bpp / 8))) = p1.color;
+        error2 = error * 2;
+        if (error2 > -dy)
+        {
+            error -= dy;
+            p1.x += sign_x;
+        }
+        if (error2 < dx)
+        {
+            error += dx;
+            p1.y += sign_y;
+        }
+    }
 }
 
 
 void	draw(t_var *data)
 {
+
 	int row;
 	int column;
-
+/* 	int pixel;
+	int color;
+	char *image_data;
+	int bpp, size_line, endian;
+	data->image.img = mlx_new_image(data->display, WIDTH, HEIGHT);
+	image_data = mlx_get_data_addr(data->image.img, &bpp, &size_line, &endian);
+ */
 
 	create_2d(data);
 	row = -1;
@@ -62,10 +60,24 @@ void	draw(t_var *data)
 		column = -1;
 		while (++column < data->width)
 		{
-			if (column != data->width - 1)
-				draw_line(data->map_2d[row][column], data->map_2d[row][column + 1], data);
-			if (row != data->height - 1)
-				draw_line(data->map_2d[row][column], data->map_2d[row + 1][column], data);
+/* 			int x = (int)data->map_2d[row][column].x;
+			int y = (int)data->map_2d[row][column].y;
+			color = data->map_2d[row][column].color;
+			
+			
+			if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
+			{
+				pixel = (x * bpp / 8) + (y * size_line);
+				image_data[pixel] = color & 0xFF;
+				image_data[pixel + 1] = (color >> 8) & 0xFF;
+				image_data[pixel + 2] = (color >> 16) & 0xFF;
+				image_data[pixel + 3] = (color >> 24) & 0xFF; 
+			}			*/
+			 if (column < data->width - 1)
+				draw_line(data->map[row][column], data->map[row][column + 1], data);
+			if (row < data->height - 1)
+				draw_line(data->map[row][column], data->map[row + 1][column], data);
 		}
 	}
+	mlx_put_image_to_window(data->display, data->window, data->image.img, 0, 0);
 }
