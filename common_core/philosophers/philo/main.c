3fe768f54bc00d60d0af382f12a2d308d97df058
cseriildi <cseriildiko@icloud.com>
2024-06-24 15:43:51 +0200

    started working with threads

--- common_core/philosophers/philo/main.c
+++ common_core/philosophers/philo/main.c
@@ -6,8 +6,23 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 13:16:59 by icseri            #+#    #+#             */
-/*   Updated: 2024/06/24 13:22:15 by icseri           ###   ########.fr       */
+/*   Updated: 2024/06/24 15:17:52 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void *func()
+{
+
+}
+
+int	main(int argc, char **argv)
+{
+	pthread_t	t1;
+
+	if (pthread_create(&t1, NULL, &func, NULL) != 0)
+		exit(1);
+	if (pthread_join(t1, NULL) != 0)
+		exit(2);
+}
