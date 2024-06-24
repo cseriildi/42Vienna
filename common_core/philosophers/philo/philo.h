e3e93de3478f263f78cd28bbcac5e2befed6bff9
icseri <icseri@student.42vienna.com>
2024-06-24 15:43:51 +0200

    started working with threads

--- common_core/philosophers/philo/philo.h
+++ common_core/philosophers/philo/philo.h
@@ -6,11 +6,16 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 13:20:04 by icseri            #+#    #+#             */
-/*   Updated: 2024/06/24 13:22:17 by icseri           ###   ########.fr       */
+/*   Updated: 2024/06/24 13:56:58 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+# include <pthread.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
 
 #endif
