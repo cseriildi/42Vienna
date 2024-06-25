8177cc8755ba341820eba769248a552c6c8dab24
cseriildi <cseriildiko@icloud.com>
2024-06-25 10:42:22 +0200

    creating threads for each philo, trying out a mutex

--- common_core/philosophers/philo/main.c
+++ common_core/philosophers/philo/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cseriildii <cseriildii@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 13:16:59 by icseri            #+#    #+#             */
-/*   Updated: 2024/06/24 15:17:52 by icseri           ###   ########.fr       */
+/*   Updated: 2024/06/25 10:21:38 by cseriildii       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,39 @@
 
 void *func()
 {
+	pthread_mutex_lock(&data->mutex);
+	//do smth
+	pthread_mutex_unlock(&data->mutex);
+}
 
+void	init_philos(t_philo *data)
+{
+	int			i;
+
+	data->philos = malloc(sizeof(pthread_t) * data->count);
+	if (!data->philos)
+		safe_exit(data, MALLOC_FAIL);
+	i = -1;
+	while (++i < data->count)
+	{
+		if (pthread_create(&data->philos[i], NULL, &func, NULL) != 0)
+			safe_exit(data, THREAD_CREATE_FAIL);
+	}
+	i = -1;
+	while (++i < data->count)
+	{
+		if (pthread_join(data->philos[i], NULL) != 0)
+			safe_exit(data, THREAD_JOIN_FAIL);
+	}
 }
 
 int	main(int argc, char **argv)
 {
-	pthread_t	t1;
+	t_philo *data;
 
-	if (pthread_create(&t1, NULL, &func, NULL) != 0)
-		exit(1);
-	if (pthread_join(t1, NULL) != 0)
-		exit(2);
+	data = malloc(sizeof(t_philo));
+	data->count = atoi(argv[1]);
+	pthread_mutex_init(&data->mutex, NULL);
+	init_philos(data);
+	pthread_mutex_destroy(&data->mutex);
 }
