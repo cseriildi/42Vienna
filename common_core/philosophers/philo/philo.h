25f7d923e1c6c5cac94a79ad6905b0aa8dd7b2c4
icseri <icseri@student.42vienna.com>
2024-06-25 17:13:33 +0200

    input checking & parsing is done, checked out gettimeofday

--- common_core/philosophers/philo/philo.h
+++ common_core/philosophers/philo/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cseriildii <cseriildii@student.42.fr>      +#+  +:+       +#+        */
+/*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 13:20:04 by icseri            #+#    #+#             */
-/*   Updated: 2024/06/25 10:16:57 by cseriildii       ###   ########.fr       */
+/*   Updated: 2024/06/25 17:01:04 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <stdbool.h>
+# include <sys/time.h>
 
 typedef enum s_err
 {
 	MALLOC_FAIL = 2,
-	WRONG_INPUT,
+	MISUSE,
+	INCORRECT_INPUT,
 	THREAD_CREATE_FAIL,
 	THREAD_JOIN_FAIL
 }	t_err;
@@ -31,8 +34,19 @@ typedef struct s_philo
 	pthread_t		*philos;
 	pthread_mutex_t	mutex;
 	int				count;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	bool			min_eat;
+	int				eat_count;
+	int				err;
 }	t_philo;
 
-void	safe_exit(t_philo *data, int error_code);
+//utils
+int		ft_atoi(const char *nptr);
+char	*ft_itoa(int nb);
+int		ft_strcmp(char *s1, char *s2);
+void	print_error(int code);
+void	free_data(t_philo *data, int code);
 
 #endif
