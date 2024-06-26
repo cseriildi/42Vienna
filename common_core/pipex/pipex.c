041b735c98c1d98c1196241286c6a13266a0bccc
cseriildi <cseriildiko@icloud.com>
2024-06-26 14:37:52 +0200

    fixed it, now the child processes are running parallel and the parent waits for them to finish

--- common_core/pipex/pipex.c
+++ common_core/pipex/pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cseriildii <cseriildii@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:57:19 by icseri            #+#    #+#             */
-/*   Updated: 2024/06/26 13:51:10 by icseri           ###   ########.fr       */
+/*   Updated: 2024/06/26 14:28:09 by cseriildii       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,8 @@ int	main(int argc, char **argv, char **env)
         elegant_exit(data, FORK_FAIL);
     else if (data->pid2 == 0)
 		last_command(data);
+	safe_close(data->pipe[0]);
+	safe_close(data->pipe[1]);
 	while (wait(&status) > 0);
 	elegant_exit(data, EXIT_SUCCESS);
 }
