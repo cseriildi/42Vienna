454e5f4fcd1f287c5288f374006b4f5353043ded
cseriildi <cseriildiko@icloud.com>
2024-09-11 20:01:38 +0200

    removing the reference to the string when returning local variable

--- common_core/cpp00/ex01/utils.hpp
+++ common_core/cpp00/ex01/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 11:15:48 by cseriildii        #+#    #+#             */
-/*   Updated: 2024/09/10 11:30:33 by icseri           ###   ########.fr       */
+/*   Updated: 2024/09/11 19:21:48 by icseri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@
 void				add_contact(PhoneBook &myPhoneBook);
 void				search_contact(PhoneBook &myPhoneBook);
 
-const std::string& get_data(const std::string& field);
+const std::string 	get_data(const std::string& field);
 int 				get_index(void);
-const std::string&	trim(const std::string& str);
+const std::string	trim(const std::string& str);
 bool				valid_phone_number(const std::string& phoneNumber);
 int					ft_atoi(const std::string& num);
 
