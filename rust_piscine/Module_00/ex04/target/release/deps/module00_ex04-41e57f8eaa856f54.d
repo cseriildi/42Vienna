bf4f282b9303873ff4b5a3eb4e3dcfb8584bdfde
cseriildi <cseriildiko@icloud.com>
2024-10-08 09:21:59 +0200

    remove unnecessary files

--- rust_piscine/Module_00/ex04/target/release/deps/module00_ex04-41e57f8eaa856f54.d
+++
@@ -1,5 +0,0 @@
-/sgoinfre/icseri/42Vienna/rust_piscine/Module_00/ex04/target/release/deps/module00_ex04-41e57f8eaa856f54: src/main.rs
-
-/sgoinfre/icseri/42Vienna/rust_piscine/Module_00/ex04/target/release/deps/module00_ex04-41e57f8eaa856f54.d: src/main.rs
-
-src/main.rs:
--- rust_piscine/Module_00/ex04/target/release/deps/other-8774de786a6c1be1.d
+++
@@ -1,5 +0,0 @@
-/sgoinfre/icseri/42Vienna/rust_piscine/Module_00/ex04/target/release/deps/other-8774de786a6c1be1: src/other.rs
-
-/sgoinfre/icseri/42Vienna/rust_piscine/Module_00/ex04/target/release/deps/other-8774de786a6c1be1.d: src/other.rs
-
-src/other.rs:
