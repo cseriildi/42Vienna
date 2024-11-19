aeec4750f6a6eceeea0c6cd75c464848030b485b
icseri <icseri@student.42vienna.com>
2024-11-19 12:33:53 +0100

    this is still not done, I have to fix overflow and nan

--- common_core/CPP_Modules/cpp02/ex03/bsp.cpp
+++ common_core/CPP_Modules/cpp02/ex03/bsp.cpp
@@ -3,23 +3,23 @@
 #include "Point.hpp"
 #include "Fixed.hpp"
 
-Fixed	cross_product(Point const& a, Point const& b, Point const& p)
+float	cross_product(Point const& a, Point const& b, Point const& p)
 {
-	Fixed const ax = a.get_x();
-	Fixed const ay = a.get_y();
-	Fixed const bx = b.get_x();
-	Fixed const by = b.get_y();
-	Fixed const px = p.get_x();
-	Fixed const py = p.get_y();
+	float const ax = a.get_x().toFloat();
+	float const ay = a.get_y().toFloat();
+	float const bx = b.get_x().toFloat();
+	float const by = b.get_y().toFloat();
+	float const px = p.get_x().toFloat();
+	float const py = p.get_y().toFloat();
 
 	return (bx - ax) * (py - ay) - (by - ay) * (px - ax);
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point) // NOLINT(performance-unnecessary-value-param)
 {
-	Fixed const abp = cross_product(a, b, point);
-	Fixed const bcp = cross_product(b, c, point);
-	Fixed const cap = cross_product(c, a, point);
+	float const abp = cross_product(a, b, point);
+	float const bcp = cross_product(b, c, point);
+	float const cap = cross_product(c, a, point);
 
 	return (abp > 0 && bcp > 0 && cap > 0) || (abp < 0 && bcp < 0 && cap < 0);
 }
