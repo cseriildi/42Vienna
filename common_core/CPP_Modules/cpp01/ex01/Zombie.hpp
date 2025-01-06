f01e138eb93bf15812bc73af3787bceecb42a58e
icseri <icseri@student.42vienna.com>
2025-01-06 15:01:17 +0100

    refactor

--- common_core/CPP_Modules/cpp01/ex01/Zombie.hpp
+++ common_core/CPP_Modules/cpp01/ex01/Zombie.hpp
@@ -19,9 +19,7 @@ class Zombie
 		
 };
 
-Zombie				*zombieHorde(int N, std::string name);
-std::string			ft_itoa(int n);
-int					ft_atoi(const std::string& num);
-bool				is_valid_input(int argc, char **argv);
+Zombie		*zombieHorde(int N, std::string name);
+std::string	ft_itoa(int n);
 
 #endif
--- common_core/CPP_Modules/cpp01/ex01/main.cpp
+++ common_core/CPP_Modules/cpp01/ex01/main.cpp
@@ -5,67 +5,34 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <cstdlib>
 
 int		main(int argc, char **argv)
 {
-	if (!is_valid_input(argc, argv))
+	if (argc != 3)
+	{
+		std::cout << "Usage: " << argv[0] << " <number of zombies to create (range int)> <name>\n";
 		return 1;
+	}
 
-	Zombie	*zombies = zombieHorde(ft_atoi(argv[1]), argv[2]);
-	for (int i = 0; i < ft_atoi(argv[1]); i++)
+	int number_of_zombies = atoi(argv[1]);
+	if (*argv[1] == '+')
+		(argv[1])++;
+	if (number_of_zombies < 1 || std::string(argv[1]) != ft_itoa(number_of_zombies))
+	{
+		std::cout << "Invalid number of zombies.\n";
+		return 1;
+	}
+
+	std::string	name = argv[2];
+	Zombie	*zombies = zombieHorde(number_of_zombies, name);
+
+	for (int i = 0; i < number_of_zombies; i++)
 		zombies[i].announce();
 
 	delete[] zombies;
 	
 	return 0;
-}
-
-bool	is_valid_input(int argc, char **argv)
-{
-	if (argc != 3)
-	{
-		std::cout << "Usage: " << argv[0] << " <number of zombies to create (range int)> <name>\n";
-		return false;
-	}
-	for  (int i = 0; argv[1][i] != '\0'; i++)
-	{
-		if (std::isdigit(argv[1][i]) == 0)
-		{
-			std::cout << "Invalid number of zombies.\n";
-			return false;
-		}
-	}
-	if (std::string(argv[1]) != std::string(ft_itoa(ft_atoi(argv[1]))))
-	{
-		std::cout << "Invalid number of zombies.\n";
-		return false;
-	}
-	if (ft_atoi(argv[1]) < 1)
-	{
-		std::cout << "Cmon let's create at least 1 zombie\n";
-		return false;
-	}
-	return true;
-}
-
-int	ft_atoi(const std::string& num)
-{
-	int result = 0;
-	int sign = 1;
-	std::string::const_iterator it = num.begin();
-
-	if (*it == '-' || *it == '+')
-	{
-		if (*it == '-')
-			sign = -1;
-		it++;
-	}
-	while (it != num.end() && std::isdigit(*it) != 0)
-	{
-		result = result * 10 + *it - '0'; //NOLINT
-		it++;
-	}
-	return result * sign;
 }
 
 std::string	ft_itoa(int n)
