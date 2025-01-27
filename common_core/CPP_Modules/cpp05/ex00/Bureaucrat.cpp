e5df3358dae4330c9c520613b39b837564dbd269
icseri <icseri@student.42vienna.com>
2025-01-27 18:07:17 +0100

    refactor

--- common_core/CPP_Modules/cpp05/ex00/Bureaucrat.cpp
+++ common_core/CPP_Modules/cpp05/ex00/Bureaucrat.cpp
@@ -14,7 +14,7 @@ Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name), _grade
 
 Bureaucrat::~Bureaucrat() {}
 
-Bureaucrat::Bureaucrat(const Bureaucrat &copy) : _name(copy._name), _grade(copy._grade) {}
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : _name(other._name), _grade(other._grade) {}
 
 Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
 {
