fc6a99b5472554d98b40b9737764a06217736c73
icseri <icseri@student.42vienna.com>
2025-03-04 15:18:34 +0100

    🎨 style: switching to pragma once
    https://www.learncpp.com/cpp-tutorial/header-guards/

--- common_core/CPP_Modules/cpp06/ex00/ScalarConverter.hpp
+++ common_core/CPP_Modules/cpp06/ex00/ScalarConverter.hpp
@@ -1,5 +1,4 @@
-#ifndef SCALARCONVERTER_HPP
-#define SCALARCONVERTER_HPP
+#pragma once
 
 #include <string>
 
@@ -16,5 +15,3 @@ class ScalarConverter
 		ScalarConverter(const ScalarConverter &other);
 		ScalarConverter &operator=(const ScalarConverter &other);
 };
-
-#endif
