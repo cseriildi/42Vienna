17cbc5a8dbde0ccb5d4dc8a3be635d85f754396f
icseri <icseri@student.42vienna.com>
2025-03-22 13:53:02 +0100

    ✨ feat: add iter func template

--- common_core/CPP_Modules/cpp07/ex01/iter.hpp
+++ common_core/CPP_Modules/cpp07/ex01/iter.hpp
@@ -1,2 +1,37 @@
 #pragma once
 
+#include <iostream>
+
+/* template <typename T, typename F>
+void iter(T *arr, size_t length, F func)
+{
+	for (size_t i = 0; i < length; i++)
+	{
+		func(arr[i]);
+	}
+} */
+
+template <typename T>
+void iter(T *arr, size_t length, void (*func)(const T&))
+{
+	for (size_t i = 0; i < length; i++)
+	{
+		func(arr[i]);
+	}
+}
+
+template <typename T>
+void iter(T *arr, size_t length, void (*func)(T*))
+{
+	for (size_t i = 0; i < length; i++)
+	{
+		func(&arr[i]);
+	}
+}
+
+template <typename T>
+void display(const T& t)
+{
+	std::cout << t << "\n";
+}
+
