ed70db85c2737ee1339bd9a489437b0e66244cae
icseri <icseri@student.42vienna.com>
2025-03-22 13:54:17 +0100

    🧪 test: add test cases

--- common_core/CPP_Modules/cpp07/ex01/main.cpp
+++ common_core/CPP_Modules/cpp07/ex01/main.cpp
@@ -1,6 +1,27 @@
 #include "iter.hpp"
+#include <cmath>
+
+void sqrt2(int *i)
+{
+	*i = std::sqrt(*i);
+}
+
+void upper(char *c)
+{
+	*c = std::toupper(*c);
+}
 
 int main(void)
 {
-	
+	int arr1[] = {42, 56, 71, 102};
+	char arr2[] = "Hello";
+
+	::iter(arr1, 4, display); //::iter(arr1, 4, display<int>);
+	::iter(arr1, 4, sqrt2);
+	::iter(arr1, 4, display);
+
+	::iter(arr2, 5, display);
+	::iter(arr2, 5, upper);
+	::iter(arr2, 5, display);
+
 }
