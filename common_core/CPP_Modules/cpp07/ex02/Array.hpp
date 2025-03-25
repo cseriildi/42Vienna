700c6e3d20bc32cad3334a2bffe8abf9c0cf4c74
icseri <icseri@student.42vienna.com>
2025-03-25 14:11:40 +0100

    🔧 chore: separate template into hpp and tpp

--- common_core/CPP_Modules/cpp07/ex02/Array.hpp
+++ common_core/CPP_Modules/cpp07/ex02/Array.hpp
@@ -18,58 +18,7 @@ class Array
 		size_t	_size;
 };
 
-template <typename T>
-Array<T>::Array(unsigned int n) : _arr(new T[n]), _size(n)
-{
-	for (unsigned int i = 0; i < n; ++i)
-		_arr[i] = T();
-}
+#include "Array.tpp"
 
 template <typename T>
-Array<T>::~Array() {delete[] _arr;}
-
-template <typename T>
-Array<T>::Array(const Array &other)
-{
-	_size = other.size();
-	_arr = new T[_size];
-	for (unsigned int i = 0; i < _size; ++i)
-		_arr[i] = other[i];
-}
-
-template <typename T>
-Array<T> &Array<T>::operator=(const Array &other)
-{
-	if (this == &other)
-		return *this;
-	delete[] _arr;
-	_size = other.size();
-	_arr = new T[_size];
-	for (unsigned int i = 0; i < _size; ++i)
-		_arr[i] = other[i];
-	return *this;
-}
-
-template <typename T>
-T &Array<T>::operator[](unsigned int i) const
-{
-	if (i >= size())
-		throw std::out_of_range("Index out of range");
-	return _arr[i];
-}
-
-template <typename T>
-unsigned int Array<T>::size(void) const {return _size;}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const Array<T>& other)
-{
-    for (unsigned int i = 0; i < other.size(); ++i)
-    {
-		if (i != 0)
-			os << ", ";
-        os << other[i];
-    }
-	os << "\n";
-    return os;
-}
+std::ostream& operator<<(std::ostream& os, const Array<T>& other);
---
+++ common_core/CPP_Modules/cpp07/ex02/Array.tpp
@@ -0,0 +1,56 @@
+#pragma once
+#include "Array.hpp"
+
+template <typename T>
+Array<T>::Array(unsigned int n) : _arr(new T[n]), _size(n)
+{
+	for (unsigned int i = 0; i < n; ++i)
+		_arr[i] = T();
+}
+
+template <typename T>
+Array<T>::~Array() { delete[] _arr; }
+
+template <typename T>
+Array<T>::Array(const Array &other) : _size(other._size), _arr(new T[other._size])
+{
+	for (unsigned int i = 0; i < _size; ++i)
+		_arr[i] = other._arr[i];
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array &other)
+{
+	if (this == &other)
+		return *this;
+	delete[] _arr;
+	_size = other._size;
+	_arr = new T[_size];
+	for (unsigned int i = 0; i < _size; ++i)
+		_arr[i] = other._arr[i];
+	return *this;
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int i) const
+{
+	if (i >= _size)
+		throw std::out_of_range("Index out of range");
+	return _arr[i];
+}
+
+template <typename T>
+unsigned int Array<T>::size(void) const { return _size; }
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const Array<T>& other)
+{
+	for (unsigned int i = 0; i < other.size(); ++i)
+	{
+		if (i != 0)
+			os << ", ";
+		os << other[i];
+	}
+	os << "\n";
+	return os;
+}
