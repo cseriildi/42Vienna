09469e28cda3a0cf4e49b430e77ed766d30b158b
icseri <icseri@student.42vienna.com>
2025-04-08 10:15:39 +0200

    ♻️ refactor: consistent indentation

--- common_core/CPP_Modules/cpp07/ex00/whatever.hpp
+++ common_core/CPP_Modules/cpp07/ex00/whatever.hpp
@@ -2,17 +2,17 @@
 
 template <typename T>
 void swap(T& a, T& b) {
-    T tmp = b;
-    b = a;
-    a = tmp;
+	T tmp = b;
+	b = a;
+	a = tmp;
 }
 
 template <typename T>
 T min(const T& a, const T& b) {
-    return a < b ? a : b;
+	return a < b ? a : b;
 }
 
 template <typename T>
 T max(const T& a, const T& b) {
-    return a > b ? a : b;
+	return a > b ? a : b;
 }
