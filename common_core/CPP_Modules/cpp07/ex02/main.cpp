574db339e352e7049c75a189d21e4be3a59143b0
icseri <icseri@student.42vienna.com>
2025-04-08 12:26:57 +0200

    🧪 test: use testcase from the subject

--- common_core/CPP_Modules/cpp07/ex02/main.cpp
+++ common_core/CPP_Modules/cpp07/ex02/main.cpp
@@ -1,13 +1,58 @@
-//NOLINTBEGIN(readability-magic-numbers)
-
-#include "Array.hpp"
+//NOLINTBEGIN
+#include <cstdlib>
+#include <ctime>
+#include <exception>
 #include <iostream>
+#include "Array.hpp"
 
+#define MAX_VAL 750
 int main(void)
 {
-	Array<int> arr(5);
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
 
-	std::cout << arr << "\n";
-	std::cout << "Size: " << arr.size() << "\n";
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;
+    return 0;
 }
-//NOLINTEND(readability-magic-numbers)
+//NOLINTEND
