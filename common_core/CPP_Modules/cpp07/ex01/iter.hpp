7e529e0ec60f10ded02ae15788a5ecb2803b4b2f
icseri <icseri@student.42vienna.com>
2025-04-08 12:25:09 +0200

    ✨ feat: getting the size of the array from the array

--- common_core/CPP_Modules/cpp07/ex01/iter.hpp
+++ common_core/CPP_Modules/cpp07/ex01/iter.hpp
@@ -1,41 +1,11 @@
-//NOLINTBEGIN(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-
 #pragma once
 
 #include <cstddef>
-#include <iostream>
 
-/* template <typename T, typename F>
-void iter(T *arr, size_t length, F func)
+template <typename T, size_t N, typename F>
+void iter(T (&array)[N], size_t length, F func)
 {
-	for (size_t i = 0; i < length; i++)
-	{
-		func(arr[i]);
-	}
-} */
-
-template <typename T>
-void iter(T *arr, size_t length, void (*func)(const T&))
-{
-	for (size_t i = 0; i < length; i++)
-	{
-		func(arr[i]);
+	for (size_t i = 0; i < (N < length ? N : length); ++i) {
+		func(*(array + i));
 	}
 }
-
-template <typename T>
-void iter(T *arr, size_t length, void (*func)(T*))
-{
-	for (size_t i = 0; i < length; i++)
-	{
-		func(&arr[i]);
-	}
-}
-
-template <typename T>
-void display(const T& t)
-{
-	std::cout << t << "\n";
-}
-
-//NOLINTEND(cppcoreguidelines-pro-bounds-pointer-arithmetic)
