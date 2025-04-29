0ee2d7bccad458136de26d6c9a2d37cabf1bd6d3
icseri <icseri@student.42vienna.com>
2025-04-29 14:11:02 +0200

    ✨ feat: add more iterators

--- common_core/CPP_Modules/cpp08/ex02/MutantStack.hpp
+++ common_core/CPP_Modules/cpp08/ex02/MutantStack.hpp
@@ -16,12 +16,23 @@ class MutantStack : public std::stack<T, Container>
 				this->c = other.c;
 			return *this;
 		}
-		
-		typedef typename Container::iterator iterator;
-		typedef typename Container::const_iterator const_iterator;
 
-		iterator begin() { return this->c.begin();}
+		typedef typename Container::iterator				iterator;
+		typedef typename Container::const_iterator			const_iterator;
+		typedef typename Container::reverse_iterator		reverse_iterator;
+		typedef typename Container::const_reverse_iterator	const_reverse_iterator;
+
+		iterator begin() {return this->c.begin();}
+		const_iterator begin() const {return this->c.begin();}
+
 		iterator end() { return this->c.end();}
+		const_iterator end() const { return this->c.end();}
+
+		reverse_iterator rbegin() {return this->rbegin();}
+		const_reverse_iterator rbegin() const {return this->c.rbegin();}
+
+		reverse_iterator rend() {return this->rend();}
+		const_reverse_iterator rend() const {return this->c.rend();}
 
 };
 
