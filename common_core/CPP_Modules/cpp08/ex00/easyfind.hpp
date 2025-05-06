7aacfbbf422f2e157e21c2b9fd4e2ce3838aeaee
icseri <icseri@student.42vienna.com>
2025-05-06 15:09:54 +0200

    ✨ feat: add non const version of easyfind

--- common_core/CPP_Modules/cpp08/ex00/easyfind.hpp
+++ common_core/CPP_Modules/cpp08/ex00/easyfind.hpp
@@ -4,6 +4,15 @@
 #include <stdexcept>
 
 template <typename T>
+typename T::iterator easyfind(T& cont, int n)
+{
+	typename T::iterator it = std::find(cont.begin(), cont.end(), n);
+	if (it == cont.end())
+		throw std::runtime_error("Element not found");
+	return it;
+}
+
+template <typename T>
 typename T::const_iterator easyfind(const T& cont, int n)
 {
 	typename T::const_iterator it = std::find(cont.begin(), cont.end(), n);
