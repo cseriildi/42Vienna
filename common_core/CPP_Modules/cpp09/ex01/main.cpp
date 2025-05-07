ad18ada4d1fba13dd8fbb65d90670f575e3a24d2
icseri <icseri@student.42vienna.com>
2025-05-07 15:07:53 +0200

    🔧 chore: add TODOs

--- common_core/CPP_Modules/cpp09/ex01/main.cpp
+++ common_core/CPP_Modules/cpp09/ex01/main.cpp
@@ -1,5 +1,22 @@
-int main(void)
+#include <iostream>
+int main(int argc, char **argv)
 {
+	if (argc != 2)
+	{
+		std::cout << "Usage: ./RPN < inverted Polish mathematical expression>\n";
+		return (1);
+	}
+	(void)argv;
+	int result = 0;
+	//TODO: parse argv[1]
+		//TODO: numbers in (0-9)
+		//TODO: operators: + - * /
+		//TODO: put numbers in a std::stack
+		//TODO: division by 0: error message
+
+	//TODO: in a while pop an element from the stack and do the operation until the stack is empty
+	
+	std::cout << "Result: " << result << "\n";
 
 }
 
