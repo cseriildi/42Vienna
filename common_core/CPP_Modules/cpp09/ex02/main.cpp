0b3dc7e9db3edb7f57ecadf153b968a17c8893e2
icseri <icseri@student.42vienna.com>
2025-05-20 16:13:57 +0200

    ✨ feat: calling the member functions in the main

--- common_core/CPP_Modules/cpp09/ex02/main.cpp
+++ common_core/CPP_Modules/cpp09/ex02/main.cpp
@@ -1,4 +1,38 @@
-int main(void)
-{
-	
+
+#include "PmergeMe.hpp"
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <ctime>
+
+int main(int argc, char **argv) try {
+	if (argc < 2)
+		throw std::invalid_argument("Usage: ./PmergeMe <positive integers>");
+
+	PmergeMe FJ(argv);
+
+	std::cout << "Before: " << FJ;
+
+	std::time_t start = std::time(0);
+	FJ.sortVec();
+	unsigned int vec_sort_time = std::time(0) - start;
+
+	FJ.empty_cache();
+
+	start = std::time(0);
+	FJ.sortDeq();
+	unsigned int deq_sort_time = std::time(0) - start;
+
+	std::cout << "After: " << FJ;
+	std::cout << "Time to process a range of " << argc - 1 << " elements with std::vector : " << vec_sort_time << " us\n";
+	std::cout << "Time to process a range of " << argc - 1 << " elements with std::deque : " << deq_sort_time << " us\n";
+
+	return 0;
+
+} catch (const std::exception &e) {
+	std::cerr << e.what() << "\n";
+	return 1;
+} catch (...) {
+	std::cout << "Unknown error\n";
+	return 1;
 }
