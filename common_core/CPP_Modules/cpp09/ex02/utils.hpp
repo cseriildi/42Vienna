5d1e0165be50f84b985ce1d6bf9bb62887a2a43e
icseri <icseri@student.42vienna.com>
2025-05-26 13:30:01 +0200

    🧪 test: add debug functions

--- common_core/CPP_Modules/cpp09/ex02/utils.hpp
+++ common_core/CPP_Modules/cpp09/ex02/utils.hpp
@@ -1,4 +1,9 @@
+#pragma once
+
+#include "../colors.hpp"
+#include <iostream>
 #include <limits>
+#include <ostream>
 #include <sstream>
 #include <string>
 #include <typeinfo>
@@ -27,4 +32,43 @@ T sto(const std::string& str) {
 		throw std::bad_cast();
 	}
 	return result;
+}
+
+template<typename Container>
+void  print2D(const Container &cont, const std::string& color = RESET)
+{
+	typedef typename Container::const_iterator OuterIt;
+    typedef typename Container::value_type InnerContainer;
+    typedef typename InnerContainer::const_iterator InnerIt;
+
+	std::cout << color << "{";
+	for (OuterIt it = cont.begin(); it != cont.end(); it++)
+	{
+		std::cout << "[";
+		for (InnerIt itt = (*it).begin(); itt != (*it).end(); itt++)
+		{
+			if (itt != (*it).begin())
+				std::cout << " ";
+			std::cout << *itt;
+		}
+		std::cout << "]";
+	}
+	std::cout << "}" RESET;
+}
+
+template<typename Container>
+void  debug(const Container &cont, const Container &unmatched)
+{
+	print2D(cont, GREEN);
+	print2D(unmatched, BLUE);
+	std::cout << "\n";
+}
+
+template<typename Container>
+void  debug(const Container &cont, const Container &cont2, const Container &unmatched)
+{
+	print2D(cont, GREEN);
+	print2D(cont2, YELLOW);
+	print2D(unmatched, BLUE);
+	std::cout << "\n";
 }
