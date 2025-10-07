ab1222d2fda45f0441b314e8fe9875cf461fed41
icseri <icseri@inception.myguest.virtualbox.org>
2025-10-07 14:12:23 +0200

    fixing nginx

--- common_core/inception/srcs/requirements/mariadb/tools/script.sh
+++ common_core/inception/srcs/requirements/mariadb/tools/script.sh
@@ -1,26 +1,10 @@
 #!/bin/sh
 
 if [ ! -d /var/lib/mysql/mysql ]; then
-    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-
-    mariadbd --skip-networking --user=mysql &
-    pid="$!"
-
-    until mariadb-admin ping --silent --socket=/run/mysqld/mysqld.sock; do
-      sleep 1
-    done
-
-    echo "Creating database and users..."
-    mariadb -u root --socket=/run/mysqld/mysqld.sock <<EOF
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-FLUSH PRIVILEGES;
-EOF
-
-    kill "$pid"
-    wait "$pid"
+  mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
 fi
+
+envsubst < /init.sql > /var/lib/mysql/init.sql
+chmod +x /var/lib/mysql/init.sql
 
 exec mariadbd-safe --datadir='/var/lib/mysql'
--- common_core/inception/srcs/requirements/nginx/tools/script.sh
+++ common_core/inception/srcs/requirements/nginx/tools/script.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-mkdir -p /etc/nginx/ssl
+mkdir -p ${CERTS}
 
 if [ ! -f ${CERTS_CRT} ] || [ ! -f ${CERTS_KEY} ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout ${CERTS_KEY} \
-        -out ${CERTS_CRT} \
+        -keyout "${CERTS_KEY}" \
+        -out "${CERTS_CRT}" \
         -subj "/C=AT/ST=Vienna/L=Vienna/O=42Vienna/CN=${DOMAIN_NAME}"
-    
-    chmod 600 ${CERTS_KEY}
-    chmod 644 ${CERTS_CRT}
+
+    chmod 600 "${CERTS_KEY}"
+    chmod 644 "${CERTS_CRT}"
 fi
 
-envsubst < /nginx.conf > /etc/nginx/nginx.conf
+envsubst '$DOMAIN_NAME $CERTS_CRT $CERTS_KEY $PHP_HOST $PHP_PORT' < /nginx.conf > /etc/nginx/nginx.conf
 
 nginx -g "daemon off;"
