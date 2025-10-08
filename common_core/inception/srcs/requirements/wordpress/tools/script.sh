d84b1d2cccae80cc8d4d0d79880694243350acc2
Ildiko Cseri <cseriildiko@icloud.com>
2025-10-08 11:52:27 +0200

    healthchecks added

--- common_core/inception/srcs/requirements/wordpress/tools/script.sh
+++ common_core/inception/srcs/requirements/wordpress/tools/script.sh
@@ -3,10 +3,6 @@
 if [ ! -f wp-config.php ]; then
     php83 /wp core download --allow-root
 
-    until mysql -h mariadb -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" -e "SELECT 1;" > /dev/null 2>&1; do
-        sleep 2
-    done
-
     php83 /wp config create \
       --dbname="$MYSQL_DATABASE" \
       --dbuser="$MYSQL_USER" \
